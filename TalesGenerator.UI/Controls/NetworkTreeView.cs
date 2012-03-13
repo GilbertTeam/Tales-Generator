5c2c4ed97d0b27ee6331f5c4212773b831001213
AlexOvchinin <alex.ovchinin@gmail.com>
2012-03-14 00:42:02 +0500

    #7

---
+++ TalesGenerator.UI/Controls/NetworkTreeView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Windows;
+using System.Windows.Controls;
+
+using TalesGenerator.Core;
+using TalesGenerator.Core.Collections;
+using TalesGenerator.UI.Properties;
+using TalesGenerator.UI.Classes;
+
+namespace TalesGenerator.UI.Controls
+{
+	class NetworkTreeView : TreeView
+	{
+		Network _network;
+	}
+}
